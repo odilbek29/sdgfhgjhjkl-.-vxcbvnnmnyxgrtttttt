e30833afff8377bed8b5a3473165e77c8c2e8463
odilbek29 <kamoliddinpardaev272@gmail.com>
2025-02-24 16:40:42 +0500

    Add files via upload

---
+++ odilbek.js
@@ -0,0 +1,9 @@
+let randomSon=Math.floor(Math.random()*100);
+
+alert("Sizda random son chiqishni boshlaydi: 😊");
+
+alert("Random orqali chiqali chiqqan son: " +randomSon);
+confirm("Biz rzndom sonni 2ga ko'paytirib bo'lmoqchimiz ?")
+
+alert(`Random orqali chiqqan son: ${randomSon}\n  Random sonni 2ga ko'paytirdik :${randomSon*2} \n  Random sonni 2ga bo'ldik :${randomSon/2} \nRandom sonni 3ga Qoldiqli bo'ldik :${randomSon%2} \n
+    `)
